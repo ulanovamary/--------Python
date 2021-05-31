a648813d36dfe9e9e3297fcb05c4ccf5f0294827
Mary <ulanovamary@yandex.ru>
2021-05-31 21:04:48 +0300

    Les1 files

--- les1_ht_6.py
+++ les1_ht_6.py
@@ -1,9 +1,25 @@
 '''
 Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет»,
- «декоратор». Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и
+ «декоратор».
+ Проверить кодировку файла по умолчанию.
+ Принудительно открыть файл в формате Unicode и
  вывести его содержимое
 '''
-with open(test_file.txt) as f_n:
-    for el_str in f_n
-        print(el_str)
+import chardet
 
+text = ['сетевое программирование', 'сокет', 'декоратор']
+with open('test_file.txt', 'w' ) as f:
+    for el in text:
+        f.write(el + ' ')
+
+neededFile = open("test_file.txt", 'rb')
+rawdata = neededFile.read()
+result = chardet.detect(rawdata)
+charenc = result['encoding']
+print(result)
+neededFile.close()
+
+with open('test_file.txt', 'r') as f:
+    line = f.readline()
+    li = line.encode(encoding='utf-8').decode(encoding='utf-8')
+    print(li)
