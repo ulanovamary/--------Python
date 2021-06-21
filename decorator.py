7aa74c8abff33a3814a4aa43797ee44ed56f3326
Mary <ulanovamary@yandex.ru>
2021-06-21 13:56:13 +0300

    6 les ht files apdated

--- decorator.py
+++ decorator.py
@@ -25,22 +25,30 @@ formatter = logging.Formatter(log_format)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-class LogDecorator(object):
-    def __init__(self):
-        self.logger = logging.getLogger('decorator-log')
-    def __call__(self, fn):
-        @functools.wraps(fn)
-        def decorated(self=None, *args, **kwargs):
-            try:
-                self.logger.debug(f'{fn.__name__} - {args} - {kwargs}')
-                result = fn(*args, **kwargs)
-                self.logger.debug(result)
-                return result
-            except Exception as e:
-                self.logger.debug("Exception {0}".format(e))
-                raise e
-            return result
-        return decorated
+def log(func_to_log):
+    def log_saver(*args, **kwargs):
+        ret = func_to_log(*args, **kwargs)
+        logger.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
+                     f'Вызов из модуля {func_to_log.__module__}.')
+        return ret
+    return log_saver
 
+
+#class LogDecorator(object):
+#    def __init__(self):
+#        self.logger = logging.getLogger('decorator-log')
+#    def __call__(self, fn):
+#        @functools.wraps(fn)
+#        def decorated(self=None, *args, **kwargs):
+#            try:
+#                self.logger.debug(f'{fn.__name__} - {args} - {kwargs}')
+#                result = fn(*args, **kwargs)
+#                self.logger.debug(result)
+#                return result
+#            except Exception as e:
+#                self.logger.debug("Exception {0}".format(e))
+#                raise e
+#            return result
+#        return decorated
 
 
---
+++ server.py
@@ -0,0 +1,58 @@
+from socket import  *
+import pickle
+import argparse
+import logging
+import log.server_log_config
+from  decorator import log
+
+
+logger = logging.getLogger('msnger_server')
+
+
+s = socket(AF_INET, SOCK_STREAM)
+
+@log
+def start_soc(addr='', port=7777):
+    s.bind((addr, port))
+    s.listen(6)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    try:
+        s.listen()
+    except Exception as e:
+        logger.critical(f'Ошибка инициализации сервера {e}')
+    else:
+        logger.info('Инициализация сервера прошла успешно')
+
+
+@log
+def arg_parse(ADDRESS='', PORT=7777):
+    parser = argparse.ArgumentParser(description='IP,PORT parser')
+    parser.add_argument("-addr", default=ADDRESS)
+    parser.add_argument("-port", default=PORT, type=int)
+    return parser.parse_args()
+
+
+def main():
+    while True:
+        client, addr = s.accept()
+        print("Получен запрос на соединение от %s" % str(addr))
+        data = client.recv(1024)
+        response = {
+            'response': 200,
+        }
+        client.send(pickle.dumps(response))
+        client.close()
+
+
+if __name__ == '__main__':
+    try:
+        socket = start_soc()
+    except Exception as e:
+        logger.critical(f'Ошибка инициализации сервера {e}')
+    else:
+        logger.info('Инициализация сервера прошла успешно')
+
+    try:
+        main()
+    except Exception as e:
+        print('Server did not start')
